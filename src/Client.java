990ce7b10a5adef65606048df3b3f21688801629
Justin <462499571@qq.com>
2018-03-15 19:22:40 +0800

    从网上重新找的代码

--- src/Client.java
+++ src/Client.java
@@ -1,62 +1,72 @@
-import java.io.*;
+// A Java program for a Client
 import java.net.*;
-public class Client {
-    public static void main(String[] args) {
-// declaration section:
-// smtpClient: our client socket
-// os: output stream
-// is: input stream
-        Socket smtpSocket = null;
-        DataOutputStream os = null;
-        DataInputStream is = null;
-// Initialization section:
-// Try to open a socket on port 25
-// Try to open input and output streams
-        try {
-            smtpSocket = new Socket("hostname", 25);
-            os = new DataOutputStream(smtpSocket.getOutputStream());
-            is = new DataInputStream(smtpSocket.getInputStream());
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: hostname");
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to: hostname");
+import java.io.*;
+
+public class Client
+{
+    // initialize socket and input output streams
+    private Socket socket            = null;
+    //private DataInputStream  input   = null;
+    private BufferedReader   input   = null;
+    private DataOutputStream out     = null;
+
+    // constructor to put ip address and port
+    public Client(String address, int port)
+    {
+        // establish a connection
+        try
+        {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            //input  = new DataInputStream(System.in);
+            input = new BufferedReader ( new InputStreamReader ( System.in ) );
+
+            // sends output to the socket
+            out    = new DataOutputStream(socket.getOutputStream());
         }
-// If everything has been initialized then we want to write some data
-// to the socket we have opened a connection to on port 25
-        if (smtpSocket != null && os != null && is != null) {
-            try {
-// The capital string before each colon has a special meaning to SMTP
-// you may want to read the SMTP specification, RFC1822/3
-                os.writeBytes("HELO\n");
-                os.writeBytes("MAIL From: k3is@fundy.csd.unbsj.ca\n");
-                os.writeBytes("RCPT To: k3is@fundy.csd.unbsj.ca\n");
-                os.writeBytes("DATA\n");
-                os.writeBytes("From: k3is@fundy.csd.unbsj.ca\n");
-                os.writeBytes("Subject: testing\n");
-                os.writeBytes("Hi there\n"); // message body
-                os.writeBytes("\n.\n");
-                os.writeBytes("QUIT");
-// keep on reading from/to the socket till we receive the "Ok" from SMTP,
-// once we received that then we want to break.
-                String responseLine;
-                while ((responseLine = is.readLine()) != null) {
-                    System.out.println("Server: " + responseLine);
-                    if (responseLine.indexOf("Ok") != -1) {
-                        break;
-                    }
-                }
-// clean up:
-// close the output stream
-// close the input stream
-// close the socket
-                os.close();
-                is.close();
-                smtpSocket.close();
-            } catch (UnknownHostException e) {
-                System.err.println("Trying to connect to unknown host: " + e);
-            } catch (IOException e) {
-                System.err.println("IOException:  " + e);
+        catch(UnknownHostException u)
+        {
+            System.out.println(u);
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+
+        // string to read message from input
+        String line = "";
+
+        // keep reading until "Over" is input
+        while (!line.equals("Over"))
+        {
+            try
+            {
+                line = input.readLine();
+                out.writeUTF(line);
+            }
+            catch(IOException i)
+            {
+                System.out.println(i);
             }
         }
+
+        // close the connection
+        try
+        {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 5000);
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,39 +1,62 @@
-import java.io.*;
+// A Java program for a Server
 import java.net.*;
-public class Server {
-    public static void main(String args[]) {
-// declaration section:
-// declare a server socket and a client socket for the server
-// declare an input and an output stream
-        ServerSocket echoServer = null;
-        String line;
-        DataInputStream is;
-        PrintStream os;
-        Socket clientSocket = null;
-// Try to open a server socket on port 9999
-// Note that we can't choose a port less than 1023 if we are not
-// privileged users (root)
-        try {
-            echoServer = new ServerSocket(9999);
-        }
-        catch (IOException e) {
-            System.out.println(e);
-        }
-// Create a socket object from the ServerSocket to listen and accept
-// connections.
-// Open input and output streams
-        try {
-            clientSocket = echoServer.accept();
-            is = new DataInputStream(clientSocket.getInputStream());
-            os = new PrintStream(clientSocket.getOutputStream());
-// As long as we receive data, echo that data back to the client.
-            while (true) {
-                line = is.readLine();
-                os.println(line);
+import java.io.*;
+
+public class Server
+{
+    //initialize socket and input stream
+    private Socket          socket   = null;
+    private ServerSocket    server   = null;
+    private DataInputStream in       =  null;
+
+    // constructor with port
+    public Server(int port)
+    {
+        // starts server and waits for a connection
+        try
+        {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+
+            System.out.println("Waiting for a client ...");
+
+            socket = server.accept();
+            System.out.println("Client accepted");
+
+            // takes input from the client socket
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+
+            String line = "";
+
+            // reads message from client until "Over" is sent
+            while (!line.equals("Over"))
+            {
+                try
+                {
+                    line = in.readUTF();
+                    System.out.println(line);
+
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
             }
+            System.out.println("Closing connection");
+
+            // close connection
+            socket.close();
+            in.close();
         }
-        catch (IOException e) {
-            System.out.println(e);
+        catch(IOException i)
+        {
+            System.out.println(i);
         }
+    }
+
+    public static void main(String args[])
+    {
+        Server server = new Server(5000);
     }
 }
